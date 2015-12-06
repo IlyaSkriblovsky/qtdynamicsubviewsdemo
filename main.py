e6e9446d3540b45fd6db54c322cef9d16726235c
Ilya Skriblovsky <IlyaSkriblovsky@gmail.com>
2015-12-06 22:56:49 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from PyQt4 import QtCore, QtGui, uic
+
+class MainWindow(QtGui.QMainWindow):
+	def __init__(self):
+		QtGui.QMainWindow.__init__(self)
+		uic.loadUi("mainwindow.ui", self)
+		self.addButton.clicked.connect(self.addButtonClicked)
+		self.subviewsLayout.setAlignment(QtCore.Qt.AlignTop)
+
+
+	def addButtonClicked(self):
+		b = SubView()
+		self.subviewsLayout.insertWidget(0, b)
+
+
+class SubView(QtGui.QWidget):
+	def __init__(self):
+		QtGui.QWidget.__init__(self)
+		uic.loadUi("subview.ui", self)
+
+
+if __name__ == '__main__':
+	import sys
+	app = QtGui.QApplication(sys.argv)
+	mw = MainWindow()
+	mw.show()
+	app.exec_()
